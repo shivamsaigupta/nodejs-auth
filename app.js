f4b2f986415fcdef69d45260664cb6b7c2281ce0
DESKTOP-MSG9PH7\Shivam <contact@shivam.me>
2020-01-21 14:34:25 +0530

    Added hashing to passwords

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -14,6 +15,9 @@ mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "mongo connection error"));
+
+// creating model here for simplicity's sake
+// in a large project, use a modular approach and import models instead
 
 const User = mongoose.model(
   "User",
@@ -28,11 +32,85 @@ app.set("views", __dirname);
 app.set("view engine", "ejs");
 
 app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return next(err);
+      }
+      if (!user) {
+        return done(null, false, { msg: "Incorrect username" });
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match
+          return done(null, user);
+        } else {
+          // passwords dont match
+          return done(null, false, { msg: "Incorrect password" });
+        }
+      });
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.render("index"));
+app.use(function(req, res, next) {
+  res.locals.currentUser = req.user;
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
 app.get("/sign-up", (req, res) => res.render("sign-up-form"));
+
+app.get("/log-out", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
+
+// Skipping validation and sanitization as we are solely focused on
+// integrating passport.js with express
+
+app.post("/sign-up", (req, res, next) => {
+  bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+    if (err) {
+      return next(err);
+    }
+    const user = new User({
+      username: req.body.username,
+      password: hashedPassword
+    });
+    user.save(err => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  });
+});
+
+app.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
 
 app.listen(3000, () => console.log("listening on port 3000!"));
